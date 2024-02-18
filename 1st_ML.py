b391a3c5a67949b84b9f9f40e7276b9de217141e
KayVeeZ <kshitijvashisth@gmail.com>
2024-02-18 19:33:19 +0000

    completed first machine learning tutorial

--- 1st_ML.py
+++ 1st_ML.py
@@ -7,7 +7,7 @@ celsius_q = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
 fahrenheit_a = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 
 for i,c in enumerate(celsius_q):
-    print("{} degrees Celsius = {} degrees Fahrenheit".format(c, fahrenheit_a[i]))
+    print(f"{c} degrees Celsius = {fahrenheit_a[i]} degrees Fahrenheit")
     
 l0 = tf.keras.layers.Dense(units=4, input_shape=[1])
 l1 = tf.keras.layers.Dense(units=4)
